95f19dc686a046e081dc67e3a98d47c6f7c623ea
Collin Donnan <collin@cd.cd>
2025-11-24 15:19:30 -0500

    Adding product data to a json

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,48 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+/*
+HTML must have <div id="root"></div>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<route />)
+
+const element = <h1> ex </h1>
+
+root.render(element)
+
+name = ex
+
+printting { ex }
+
+useState gives a component memory
+
+useEffect 
+
+life cycle - contains 3 states, mounting, updating and umounting
+
+mounting - when first component appears
+
+updating - when state changes
+
+unmounting - remove
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 My Website. All rights reserved.</p>
+    </footer>
+  )
+  }
+
+}
+
+[] <- empty dependency array means it runs once on mount
+
+[x] <- runs when x changes
+
+
+
+*/
